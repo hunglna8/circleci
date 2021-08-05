322ddbc45b97e2d2fb16750e0a08111f059729ff
hungba <lengochung.ptit@gmail.com>
2021-08-05 15:05:17 +0700

    restore testa

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello ReactJs!/i);
+  const linkElement = getByText(/Hello Hungba!/i);
   expect(linkElement).toBeInTheDocument();
 });
