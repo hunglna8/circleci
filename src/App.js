7c285c544538bab22b5ac66231c878fa94af9fc8
hungba <lengochung.ptit@gmail.com>
2021-08-05 14:50:45 +0700

    change comment1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       <h1> Hello Hungba! </h1>
+      <a className="App-link" href="https://hub.docker.com/repository/docker/lengochung1/react-docker">Please check docker image </a>
       </header>
     </div>
   );
